31d237eadfbf5d5d6b34a0eac218086a8397b939
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2021-03-24 12:27:53 +0100

    Fix the display of syntax errors

--- include/reactions/exceptions.hpp
+++ include/reactions/exceptions.hpp
@@ -7,6 +7,7 @@
 
 #include <cstring>
 #include <exception>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
@@ -14,22 +15,20 @@
  */
 namespace reactions::exceptions {
 
-  const char *mark_error(std::string const &str, const char *msg,
+  std::string mark_error(std::string const &str, const char *msg,
                          std::size_t rpos) {
 
-    auto nb = strlen(msg) + 2;
-    auto ps = strlen(msg) - rpos;
-
+    auto nb = strlen(msg) + 3;
+    auto ps = str.size() - rpos;
     std::string new_msg;
-    new_msg.reserve(2 * nb + str.size() + ps + 2);
+    new_msg.reserve(nb + str.size() + ps + 3);
     new_msg.append(msg);
-    new_msg.append(":\n"); // size 2
+    new_msg.append(":\n "); // size 3
     new_msg.append(str);
     new_msg.push_back('\n'); // size 1
-    new_msg.append(nb + ps, ' ');
+    new_msg.append(ps + 1, ' ');
     new_msg.push_back('^'); // size 1
-
-    return new_msg.c_str();
+    return new_msg;
   }
 
   class internal_error : std::runtime_error {
@@ -55,7 +54,7 @@ namespace reactions::exceptions {
     const char *what() const noexcept { return m_msg; }
 
     syntax_error update(std::string const &str) {
-      return {mark_error(str, m_msg, m_rpos)};
+      return {mark_error(str, m_msg, m_rpos).c_str()};
     }
 
   private:
