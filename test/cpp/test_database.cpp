7c8ebb83dee2faa886886d4738a76f8afd9a23ff
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2021-04-05 12:31:54 +0200

    Separate tests of elements in C++

--- test/cpp/test_database.cpp
+++ test/cpp/test_database.cpp
@@ -7,92 +7,25 @@ using namespace reactions;
 int main() {
 
   test::collector pdg_database_coll("pdg_database");
-  pdg_database_coll.add_test_function("test database", []() -> test::errors {
-    test::errors errors;
+  pdg_database_coll.add_test_function(
+      "test PDG database", []() -> test::errors {
+        test::errors errors;
 
-    try {
+        try {
 
-      if (&pdg_database::instance() != &pdg_database::instance())
-        errors.push_back("The PDG database is not a singleton");
+          if (&pdg_database::instance() != &pdg_database::instance())
+            errors.push_back("The PDG database is not a singleton");
 
-      auto &db = pdg_database::instance();
+          auto &db = pdg_database::instance();
 
-      if (db("pi+").pdg_id() != db(+211).pdg_id())
-        errors.push_back(
-            "Accessors by string and PDG ID do not retrieve the same elements");
-    }
-    REACTIONS_TEST_UTILS_CATCH_EXCEPTIONS(errors);
+          if (db("pi+").pdg_id() != db(+211).pdg_id())
+            errors.push_back("Accessors by string and PDG ID do not retrieve "
+                             "the same elements");
+        }
+        REACTIONS_TEST_UTILS_CATCH_EXCEPTIONS(errors);
 
-    return errors;
-  });
-  pdg_database_coll.add_test_function("test PDG element", []() -> test::errors {
-    test::errors errors;
-
-    try {
-
-      auto &db = pdg_database::instance();
-
-      if (db("pi+").pdg_id() != db(+211).pdg_id())
-        errors.push_back(
-            "Accessors by string and PDG ID do not retrieve the same elements");
-
-      pdg_element::base_type ta = {
-          "a", 0, 0, reactions::missing, reactions::missing, false};
-      pdg_element a = std::move(ta);
-
-      pdg_element::base_type tb = {
-          "b", 0, 0, reactions::missing, reactions::missing, false};
-      pdg_element b{std::move(tb)};
-
-      pdg_element c1 = {"c1", 0, 0, reactions::missing, reactions::missing,
-                        false};
-      pdg_element c2 = {
-          "c2", 0, 0, reactions::fill{0., 0., 0.}, reactions::missing, false};
-      pdg_element c3 = {
-          "c3", 0, 0, reactions::fill{0., 0., 0.}, reactions::fill{0., 0., 0.},
-          false};
-
-      if (c1.has_mass() || c1.has_width() || !c2.has_mass() || c2.has_width() ||
-          !c3.has_mass() || !c3.has_width())
-        errors.push_back("Problems found building custom PDG elements");
-
-      auto check_latex_name = [&db, &errors](std::string const &name,
-                                             std::string const &reference) {
-        auto const ln = db(name).latex_name();
-        if (ln != reference)
-          errors.push_back("Wrong LaTeX name for particle \"" + name +
-                           "\": \"" + ln + "\" (reference: \"" + reference +
-                           "\")");
-      };
-
-      try {
-        check_latex_name("KS0", "K_{S}^{0}");
-        check_latex_name("K+", "K^{+}");
-        check_latex_name("pi+", "\\pi^{+}");
-        check_latex_name("pi-", "\\pi^{-}");
-        check_latex_name("Lambda", "\\Lambda");
-        check_latex_name("eta'(958)", "\\eta^{'}(958)");
-        check_latex_name("a_0(980)0", "a_{0}(980)^{0}");
-        check_latex_name("f'_2(1525)", "f_{2}^{'}(1525)");
-        check_latex_name("K*_2(1430)~0", "\\bar{K}_{2}^{*}(1430)^{0}");
-        check_latex_name("D*_s2(2573)+", "D_{s2}^{*}(2573)^{+}");
-        check_latex_name("Xi'_c+", "\\Xi_{c}^{'+}");
-        check_latex_name("Delta(1950)~-", "\\bar{\\Delta}(1950)^{-}");
-      } catch (reactions::internal_error &) {
-        errors.push_back("Internal error detected processing LaTeX names");
-      }
-
-      try {
-        for (auto e : reactions::pdg_database::instance().all_elements())
-          e.latex_name();
-      } catch (reactions::internal_error &) {
-        errors.push_back("Unable to compute LaTeX names for some elements");
-      }
-    }
-    REACTIONS_TEST_UTILS_CATCH_EXCEPTIONS(errors);
-
-    return errors;
-  });
+        return errors;
+      });
   pdg_database_coll.add_test_function("test cache", []() -> test::errors {
     test::errors errors;
 
