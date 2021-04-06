d2c18297118c7ef41d8ea193aaeeb75ae1fd3e57
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2021-04-06 10:05:47 +0200

    Improve tests

--- test/python/test_units.py
+++ test/python/test_units.py
@@ -12,7 +12,9 @@ def test_pdg_system_of_units_sgl():
 
 def test_pdg_system_of_units():
     mass_prev = reactions.pdg_database('KS0').mass
+    assert reactions.pdg_system_of_units.get_energy_units() == 'GeV'
     reactions.pdg_system_of_units.set_energy_units('MeV')
+    assert reactions.pdg_system_of_units.get_energy_units() == 'MeV'
     assert abs(mass_prev - reactions.pdg_database('KS0').mass * 1e-3) < 1e-12
     with pytest.raises(reactions.ValueError):
         reactions.pdg_system_of_units.set_energy_units('unknown')
