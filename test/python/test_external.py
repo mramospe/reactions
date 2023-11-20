e2ca371ed9b969dd6699499e26ff109c381d8282
Miguel Ramos Pernas <miguel.ramos.pernas@gmail.com>
2023-11-20 15:05:48 +0000

    format

--- test/python/test_external.py
+++ test/python/test_external.py
@@ -4,4 +4,4 @@ import reactions
 
 def test_particle():
     for e in reactions.pdg_database.all_elements():
-        assert(particle.Particle.from_pdgid(e.pdg_id).name == e.name)
+        assert (particle.Particle.from_pdgid(e.pdg_id).name == e.name)
